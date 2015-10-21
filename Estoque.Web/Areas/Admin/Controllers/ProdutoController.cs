b63eb53f18ecc15ba8000b6b2d4f65b7070f656f
taysontey <taysontey@gmail.com>
2015-10-21 15:03:59 -0200

    Insert Produto/DropDown Fornecedor

--- Estoque.Web/Areas/Admin/Controllers/ProdutoController.cs
+++ Estoque.Web/Areas/Admin/Controllers/ProdutoController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Estoque.DAL.Entity;
+using Estoque.DAL.Persistence;
+using Estoque.Web.Areas.Admin.Models;
 
 namespace Estoque.Web.Areas.Admin.Controllers
 {
@@ -14,10 +17,50 @@ namespace Estoque.Web.Areas.Admin.Controllers
         {
             return View();
         }
-
+        
         public ActionResult Cadastro()
-        {
+        {        
             return View();
         }
+
+        #region Metodos AJAX
+
+        public JsonResult FillDropDownFornecedor()
+        {
+            FornecedorDal d = new FornecedorDal();
+
+            var lista = d.FindAll();
+
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Cadastrar(ProdutoModelCadastro model)
+        {
+            try
+            {
+                Funcionario u = (Funcionario)Session["funcionariologado"];
+
+                Produto p = new Produto();
+
+                p.Nome = model.Nome;
+                p.Preco = model.Preco;
+                p.Quantidade = model.Quantidade;
+                p.Descricao = model.Descricao;
+                p.IdFornecedor = model.IdFornecedor;
+
+                ProdutoDal d = new ProdutoDal();
+                d.Insert(p);
+
+                return Json("Dados gravados com sucesso.");
+            }
+            catch (Exception e)
+            {
+                return Json(e.Message);
+            }
+        }
+
+        
+
+        #endregion
     }
 }
